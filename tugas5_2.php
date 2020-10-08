7ce57b2255e99064c5a13c4b0f6cea177be7c15f
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-08 11:05:51 +0700

    Add files via upload

---
+++ tugas5_2.php
@@ -0,0 +1,30 @@
+<?php
+$belajar=array ("Ayo", "Belajar", "Bersama", "Niomic");
+
+
+
+
+echo "<h2>Operator String</h2>";
+echo "<table border =1>
+        <tr>
+            <td>Input 1</td>
+            <td>Input 2</td>
+            <td>Hasil</td>
+        </tr>
+        <tr>
+            
+            <td>$belajar[0]</td>
+            <td>$belajar[1]</td>
+            <td>$belajar[0] $belajar[1]</td>
+        </tr>
+        <tr>
+            
+            <td>$belajar[2]</td>
+            <td>$belajar[3]</td>
+            <td>$belajar[2] $belajar[3]</td>
+       
+        
+        </tr>
+      </table> <br> ";
+      echo "<br> Gabungan :",$belajar[0],$belajar[1],  $belajar[2],  $belajar[3];
+?>
